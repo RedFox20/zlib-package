cb4582eb35d6c7e7ca5d411b852f8f59bc63c83a
Jorma Rebane <jorma.rebane@gmail.com>
2023-10-29 15:01:12 +0200

    build: added package build mamafile

---
+++ mamafile.py
@@ -0,0 +1,32 @@
+import mama
+from mama.utils.system import console
+from mama.utils.gnu_project import BuildProduct
+
+# Explore Mama docs at https://github.com/RedFox20/Mama
+class zlib(mama.BuildTarget):
+
+    local_workspace = 'packages'
+
+    def init(self):
+        self.zlib = self.gnu_project('zlib', '1.3',
+            url='https://www.zlib.net/{{project}}.tar.gz',
+            build_products=[
+                BuildProduct('{{installed}}/lib/libz.a', None),
+            ])
+
+    def settings(self):
+        self.config.prefer_gcc(self.name)
+        if self.mips:
+            self.config.set_mips_toolchain('mipsel')
+
+    def build(self):
+        if self.zlib.should_build():
+            # override to always run configure
+            self.zlib.configure_command = f'configure --static --prefix {self.zlib.install_dir()}'
+            self.zlib.build()
+        else:
+            console('lib/libz.a already built', color='green')
+
+    def package(self):
+        self.export_include('zlib-built/include', build_dir=True)
+        self.export_lib('zlib-built/lib/libz.a', build_dir=True)
